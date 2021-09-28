04a6c3d1924468f2d3cbf8189a315c7e73d1c967
YazminGH <yazmin.alejandragh@gmail.com>
2021-09-28 09:31:47 -0500

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+#Product.create!(title: 'Building Blocks',
+#  description:
+#  %{Is an American cartoon launched in 2011 by Cartoon Network.},
+#  image_url: 'princess.jpg',
+#  price: 100.00)
